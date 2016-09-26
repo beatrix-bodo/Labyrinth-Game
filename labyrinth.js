2d5bb4e635dd68f105c16ac954ac418f301d66bd
Beatrix Bodo <beatrix.bodo@gmail.com>
2016-09-26 21:28:19 +0200

    Make field moveable and colourful

--- labyrinth.js
+++ labyrinth.js
@@ -1,13 +1,46 @@
 "use strict";
 
 var labyrinth = document.getElementById("labyrinth");
+var row = 10, col = 20;
 
-for (var i = 0; i < 10; i++) {
-  var html = "";
-  html += "<div class='row'>";
-  for (var j = 0; j < 20; j++) {
-    html += "<div class='field'></div>";
+var html = '';
+for(var i = 0; i < row; i++) {
+  html += ' <div class="row">';
+  for(var j = 0; j < col; j++) {
+    html += '<div class="field"></div>';
+  }
+  html += '</div>';
+}
+labyrinth.innerHTML = html;
+
+var position = 0;
+var labyrinth = document.querySelector('#labyrinth');
+var currentField = document.querySelector('.field');
+currentField.style.backgroundColor = "red";
+
+window.addEventListener('keydown',function(e) {
+  var key = e.key;
+  currentField = changeCurrentActiveField(currentField,key);
+});
+
+function changeCurrentActiveField(current, key) {
+  var allFields = document.querySelectorAll('.field');
+  switch (key) {
+    case "ArrowLeft":
+      position--;
+      break;
+    case "ArrowDown":
+      position += col;
+      break;
+    case "ArrowUp":
+      position -= col;
+      break;
+    case "ArrowRight":
+      position++;
+    break;
   };
-  html += "</div>"
-  labyrinth.innerHTML += html;
+
+  current = allFields[position];
+  current.style.backgroundColor = 'red';
+  return current;
 };
